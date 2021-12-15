cb5d0fce8563019559ffaee3f37c32b70d72b529
yinyao_yy <715130775@qq.com>
2021-12-15 16:21:02 +0800

    增加死信队列消费者

--- src/main/java/com/yinyao/rabitmq/rabbit_consumer/RabbitConsumer.java
+++ src/main/java/com/yinyao/rabitmq/rabbit_consumer/RabbitConsumer.java
@@ -31,16 +31,44 @@ public class RabbitConsumer {
             //3、进行业务处理
             System.out.println("=====进行业务处理====");
             //模拟出现异常
-            int  i = 5/0;
+//            int  i = 5/0;
             //4、采用手动ack，一条条的消费 // 消息的标识，false只确认当前一个消息收到，true确认所有consumer获得的消息（成功消费，消息从队列中删除 ）
 //           是否批量.true:将一次性ack所有小于deliveryTag的消息。
+            //false只确认签收当前的消息，设置为true的时候则代表签收该消费者所有未签收的消息
             channel.basicAck(deliveryTag, false);
         } catch (Exception e) {
             //拒绝签收
              /*
             第三个参数：requeue：重回队列。如果设置为true，则消息重新回到queue，broker会重新发送该消息给消费端
              */
-            channel.basicNack(deliveryTag, true, true);
+            channel.basicNack(deliveryTag, false, true);
+        }
+    }
+
+    /**
+     * 测试延迟死信队列
+     * @param message
+     */
+    @RabbitListener(queues = "receive_queue")
+    public void receive_queue(Message message,Channel channel) throws IOException {
+        long deliveryTag = message.getMessageProperties().getDeliveryTag();
+        try {
+            //2、获取消息
+            System.out.println("message:"+new String(message.getBody()));
+            //3、进行业务处理
+            System.out.println("=====进行业务处理====");
+            //模拟出现异常
+//            int  i = 5/0;
+            //4、采用手动ack，一条条的消费 // 消息的标识，false只确认当前一个消息收到，true确认所有consumer获得的消息（成功消费，消息从队列中删除 ）
+//           是否批量.true:将一次性ack所有小于deliveryTag的消息。
+            //false只确认签收当前的消息，设置为true的时候则代表签收该消费者所有未签收的消息
+            channel.basicAck(deliveryTag, false);
+        } catch (Exception e) {
+            //拒绝签收
+             /*
+            第三个参数：requeue：重回队列。如果设置为true，则消息重新回到queue，broker会重新发送该消息给消费端
+             */
+            channel.basicNack(deliveryTag, false, true);
         }
     }
 }
